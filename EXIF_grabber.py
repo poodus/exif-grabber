8f9cd1565f9afe62b8b837cf32cc00167ab1676f
Shane Reetz <poodus@gmail.com>
2017-09-12 08:32:38 -0700

    Improved comments

--- EXIF_grabber.py
+++ EXIF_grabber.py
@@ -5,29 +5,32 @@ from fractions import Fraction
 from PIL import Image
 from PIL.ExifTags import TAGS
 
-''' EXIF GRABBER
-Looks at .jpg files in a directory and returns a CSV with EXIF data from the images
-EXIF data included: Date/time, flash, exposure time, ISO speed, F number
-TODO make time from noon a command line flag
-TODO make cmd line option to choose which variables to capture
+''' 
+    EXIF GRABBER
+    
+    Utility to analyze .jpg files in a directory and return a CSV with EXIF data from the images.
+
+    EXIF data currently included: Date/time, flash, exposure time, ISO speed, F number
+
+    TODO make time from noon a command line flag
+    TODO make cmd line option to choose which variables to capture
+    
+    @author Shane Reetz (initially written in 2013, currently being rewritten)
 '''
 
-def isValidTime(hour, minute, second):
-    if hour > 23 or hour < 0 or minute < 0 or minute >59 or second < 0 or second > 59:
-        return 0
 
 '''
     conv_to_time_from_noon()
     
     Converts time from the format given in the file to a time relative to noon being "0:0"
-    positive values being after 12, negative values before.
+    positive values being after 12, negative values before. This was useful in the initial
+    use case of this project, for statistical analysis of time values in relation to exposure.
 '''
 def conv_to_time_from_noon(input):
+    # Get values from input.
     hour = int(input[11:13])
     minute = int(input[14:16])
     second = int(input[17:19])
-    if isValidTime(hour, minute, second) == 0:
-        return "ERROR"
     # Case 1: After Noon
     if hour >= 12 and hour <=23:
         hour = hour - 12
@@ -49,29 +52,34 @@ def conv_to_time_from_noon(input):
 '''
     get_exif()
     
-    Returns various EXIF data for a given file and adds to CSV row
+    Returns various EXIF data for a given file and adds to CSV file.
 '''
 def get_exif(filename):
     print filename
     img = Image.open(filename)
     foundattributes = 0
+    # List to append values to
     listbuild = []
     info = img._getexif()
+    # For every EXIF entry in this file...
     for tag, value in info.items():
+        # Decode EXIF code into String
         decoded = TAGS.get(tag, tag)
-        print decoded
+        # Time
         if decoded == 'DateTimeOriginal':
             if timeFromNoon:
                 listbuild.append(conv_to_time_from_noon(str(value)))
             else:
                 listbuild.append(str(value))
             foundattributes += 1
+        # Flash
         if decoded == 'Flash':
             listbuild.append(value)
             foundattributes += 1
+        # Exposure
         if decoded == 'ExposureTime':
             input = str(value)
-            # Exposure values are given as fractions. Convert them to a float.
+            # Exposure values can be expressed as fractions. Convert them to a float.
             if '/' in input:
                 list = input.split('/')
                 result = int(list[0]) / int(list[1])
@@ -80,9 +88,11 @@ def get_exif(filename):
             else:
                 listbuild.append(input)
                 foundattributes += 1
+        # ISO
         if decoded == 'ISOSpeedRatings':
             listbuild.append(str(value))
-            foundattributes += 1            
+            foundattributes += 1
+        # F Number
         if decoded == 'FNumber':
             input = int(value)
             listbuild.append(result)
@@ -90,30 +100,34 @@ def get_exif(filename):
         foundattributes = 0
     return listbuild
 
-
+# Flag for using this option.
 timeFromNoon = False
 
 '''
     main()
     
     Main application.
+    
+    # TODO command line options to add: verbose mode
 '''
 def main():
     filesProcessed = 0
+    # Get filename from user
     pathToScan = raw_input("Enter file path: ")
-    assert os.path.exists(pathToScan), "Not a valid directory: " + str(pathToScan)
+    # Validate directory
+    try:
+        assert os.path.exists(pathToScan), "Not a valid directory: " + str(pathToScan)
+    except:
+        print "Invalid input."
+        return
     csvOutput = open('EXIF_table_export.csv', 'wb')
     wr = csv.writer(csvOutput)
     print "Running..."
+    # For every file in the directory, analyze its EXIF data
     for currentfile in glob.glob(os.path.join(pathToScan, '*.*')):
-        #if os.path.isdir(currentfile):
-        #path = currentfile
-        #scan_dir(path)
         if currentfile.endswith('.JPG') or currentfile.endswith('.jpg'):
             filesProcessed += 1
-            attr = get_exif(currentfile)
-            print attr
-            wr.writerow(attr)
+            wr.writerow(get_exif(currentfile))
     print "Done! Processed " + str(filesProcessed) + " images."
 
 
